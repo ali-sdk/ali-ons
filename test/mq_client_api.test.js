322b4b4fff7ce4270cd1b64bdcd9dd87558b819b
wujia <geoff.j.wu@gmail.com>
2018-09-11 21:03:03 +0800

    add test

--- test/mq_client_api.test.js
+++ test/mq_client_api.test.js
@@ -5,10 +5,10 @@ const address = require('address');
 const httpclient = require('urllib');
 const MQClientAPI = require('../lib/mq_client_api');
 
-describe('test/mq_client_api.test.js', function() {
+describe('test/mq_client_api.test.js', function () {
   let client;
   before(() => {
-    client = new MQClientAPI(Object.assign({ httpclient }, require('../example/config')));
+    client = new MQClientAPI(Object.assign({httpclient}, require('../example/config')));
     return client.ready();
   });
 
@@ -33,4 +33,18 @@ describe('test/mq_client_api.test.js', function() {
     }
     assert(isError);
   });
+
+  it('should getTopicRouteInfoFromNameServer ok', async () => {
+    const res = await client.getTopicRouteInfoFromNameServer('TEST_TOPIC', 3000);
+    assert(res);
+  });
+
+  it('should getTopicRouteInfoFromNameServer ok if old one is closed ', async () => {
+    client._namesrvAddrList.push('1.2.3.4');
+    client._namesrvAddrList.push('6.7.8.9');
+    const res = await client.getTopicRouteInfoFromNameServer('TEST_TOPIC', 3000);
+    assert(res);
+  });
+
+
 });
