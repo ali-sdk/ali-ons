c42e902f2c92789e6c1f042f6ca93043ff66a9ea
wujia <geoff.j.wu@gmail.com>
2018-09-11 21:38:43 +0800

    jshint support

--- test/mq_client_api.test.js
+++ test/mq_client_api.test.js
@@ -5,10 +5,10 @@ const address = require('address');
 const httpclient = require('urllib');
 const MQClientAPI = require('../lib/mq_client_api');
 
-describe('test/mq_client_api.test.js', function () {
+describe('test/mq_client_api.test.js', function() {
   let client;
   before(() => {
-    client = new MQClientAPI(Object.assign({httpclient}, require('../example/config')));
+    client = new MQClientAPI(Object.assign({ httpclient }, require('../example/config')));
     return client.ready();
   });
 
@@ -39,7 +39,7 @@ describe('test/mq_client_api.test.js', function () {
     assert(res);
   });
 
-  it('should getTopicRouteInfoFromNameServer ok if old one is closed ', async () => {
+  it('should getTopicRouteInfoFromNameServer ok if old one is closed', async () => {
     client._namesrvAddrList.push('1.2.3.4');
     client._namesrvAddrList.push('6.7.8.9');
     const res = await client.getTopicRouteInfoFromNameServer('TEST_TOPIC', 3000);
