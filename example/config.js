fda994491928b91bffb268812d00c1fbaed5f092
Hongcai Deng <admin@dhchouse.com>
2019-03-12 15:17:46 +0800

    feat: support set namesrv

--- example/config.js
+++ example/config.js
@@ -11,5 +11,7 @@ module.exports = {
   producerGroup: 'PID_GXCSOCCER',
   consumerGroup: 'CID_GXCSOCCER',
   topic: 'GXCSOCCER',
+  // https://help.aliyun.com/document_detail/102895.html 阿里云产品更新，支持实例化
+  // nameSrv: '112.124.141.191:80',
   onsAddr: 'http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet',
 };
