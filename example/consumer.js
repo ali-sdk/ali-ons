0b8ea4bce3a5af20b17028f172786eb1c8d5ea44
gxcsoccer <gxcsoccer@126.com>
2019-04-12 01:23:52 +0800

    fix: drop message if reconsumeTimes > maxReconsumeTimes

--- example/consumer.js
+++ example/consumer.js
@@ -7,12 +7,14 @@ const config = require('./config');
 const consumer = new Consumer(Object.assign(config, {
   httpclient,
   logger,
+  maxReconsumeTimes: 1,
   // isBroadcast: true,
-  consumeFromWhere: 'CONSUME_FROM_LAST_OFFSET_AND_FROM_MIN_WHEN_BOOT_FIRST',
+  // consumeFromWhere: 'CONSUME_FROM_LAST_OFFSET_AND_FROM_MIN_WHEN_BOOT_FIRST',
 }));
 
 consumer.subscribe(config.topic, '*', async function(msg) {
   console.log(`receive message, msgId: ${msg.msgId}, body: ${msg.body.toString()}`)
+  // throw new Error(123);
 });
 
 consumer.on('error', err => console.log(err));
